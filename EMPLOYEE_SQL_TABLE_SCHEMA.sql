b2c1ab4ca6c1989b0047c0ca98e99fe5bc98f02d
Timothy O'Malley <tomalley@landmarkeducation.com>
2020-09-23 16:11:55 -0500

    files

---
+++ EMPLOYEE_SQL_TABLE_SCHEMA.sql
@@ -0,0 +1,44 @@
+-- DATA ENGINEERING > Creating Tables:
+
+-- CREATE TABLE departments (
+-- 	dept_no VARCHAR,
+-- 	dept_name VARCHAR
+-- );
+-- SELECT * FROM departments;
+
+-- CREATE TABLE employees (
+-- 	emp_no INT,
+-- 	emp_title_id VARCHAR,
+-- 	birth_date DATE,
+-- 	first_name VARCHAR,
+-- 	last_name VARCHAR,
+-- 	sex VARCHAR,
+-- 	hire_date DATE
+-- );
+-- SELECT * FROM employees;
+
+-- CREATE TABLE dept_emp (
+-- 	emp_no INT,
+-- 	dept_no VARCHAR
+-- );
+-- SELECT * FROM dept_emp;
+
+-- CREATE TABLE dept_managers (
+-- 	dept_no VARCHAR,
+-- 	emp_no INT
+-- );
+-- SELECT * FROM dept_managers;
+
+-- CREATE TABLE salaries (
+-- 	emp_no INT,
+-- 	salary INT
+-- );
+-- SELECT * FROM salaries;
+
+-- CREATE TABLE titles (
+-- 	title_id VARCHAR,
+-- 	title VARCHAR
+-- );
+-- SELECT * FROM salaries;
+
+
